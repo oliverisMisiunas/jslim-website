c4f05aa3cff5dc58a4a21e9f8beb56e3418d51d2
ausytesWebsite <oliverismail@gmail.com>
2022-03-22 12:14:55 +0200

    build bug fixes

--- pages/howtoopensource.js
+++ pages/howtoopensource.js
@@ -23,7 +23,7 @@ export default function howtoopensource() {
           easy to learn if you don’t already know how to contribute to
           Open-source projects. There are plenty of video tutorials (or written
           tutorials, if that’s your thing) on this subject. However, I would
-          highly recommend <a href="https://www.youtube.com/watch?v=waEb2c9NDL8" target='_blank' rel="noopener">this tutorial</a> because it explains all
+          highly recommend <a href="https://www.youtube.com/watch?v=waEb2c9NDL8" target='_blank' rel="noreferrer">this tutorial</a> because it explains all
           the needed functionality very quickly and very clearly.
         </p>
 
@@ -35,10 +35,10 @@ export default function howtoopensource() {
         <p>
           The first way is to think of what JavaScript code could be shortened
           by just writing an HTML class. I would recommend taking a look at{" "}
-          <a href="/docs" rel="noopener">JSlim classes</a> first, just to get a better understanding
+          <a href="/docs" rel="noreferrer">JSlim classes</a> first, just to get a better understanding
           of what are and how they work, and if your idea isn’t already
           implemented. It might also help you to get into the right head-space
-          to think of new ideas. After that, you should take a look at the <a href="https://github.com/oliverisMisiunas/jslim-library/blob/master/index.js" target='_blank' rel="noopener">source code</a> to familiarize yourself with the JSlim
+          to think of new ideas. After that, you should take a look at the <a href="https://github.com/oliverisMisiunas/jslim-library/blob/master/index.js" target='_blank' rel="noreferrer">source code</a> to familiarize yourself with the JSlim
           conventions. Don’t worry, the source code is fairly short and should
           be easy to read and understand. Now, once your idea is transformed
           into JavaScript code, you can follow these steps to submit it:
@@ -46,7 +46,7 @@ export default function howtoopensource() {
 
         <ol>
           <li>
-            Fork <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-library" rel="noopener">Jslim GitHub repo</a>.
+            Fork <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-library" rel="noreferrer">Jslim GitHub repo</a>.
           </li>
           <li>Go to the index.js file and write your class at the bottom.</li>
           <li>Make a Pull Request.</li>
@@ -57,7 +57,7 @@ export default function howtoopensource() {
           your class. This way your class will be added to JSlim much faster!
           The documentation can be a short text file or a Pull Request comment,
           no need to do any text formation. If you chose to write a text file,
-          you can commit it to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-documentation" rel="noopener">this repository</a> by making a Pull
+          you can commit it to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-documentation" rel="noreferrer">this repository</a> by making a Pull
           Request. If English isn’t your first language, don’t worry, all
           grammatical errors will be corrected before publishing. Whether you
           choose to write the documentation or not, your name and a link to your
@@ -66,10 +66,10 @@ export default function howtoopensource() {
         </p>
 
         <p>
-          Another great way to contribute is to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-library/issues" rel="noopener">create issues</a>.
+          Another great way to contribute is to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-library/issues" rel="noreferrer">create issues</a>.
           Address any bug or incompatibility you notice. You can also create
           issues to request a feature. If you have noticed a bug on this
-          website, you can go to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-website" rel="noopener">its repository</a> and create an
+          website, you can go to <a target='_blank' href="https://github.com/oliverisMisiunas/jslim-website" rel="noreferrer">its repository</a> and create an
           issue there. They are very highly appreciated.
         </p>
 
