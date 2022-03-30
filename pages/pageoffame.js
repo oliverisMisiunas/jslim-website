c05d821ff71f04e62eec76750aa637693a303945
Oliveris Misiūnas <99014259+oliverisMisiunas@users.noreply.github.com>
2022-03-30 12:52:24 +0300

    Update pageoffame.js

--- pages/pageoffame.js
+++ pages/pageoffame.js
@@ -11,6 +11,8 @@ export default function pageoffame() {
             <a target='_blank' rel="noreferrer" href="https://github.com/blesuer0729">Brennan LeSuer</a>
             <a target='_blank' rel="noreferrer" href="https://github.com/tomasvn">tomasvn</a>
             <a target='_blank' rel="noreferrer" href="https://github.com/eknoorpreet">Eknoorpreet Singh</a>
+            <a target='_blank' rel="noreferrer" href="https://github.com/harry-whorlow">Harry Whorlow</a>
+            <a target='_blank' rel="noreferrer" href="https://github.com/timmy-83">timmy-83</a>
         </div>
 
         <Bottom />
